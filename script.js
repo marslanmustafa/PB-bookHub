0b33fa28ebc4bc204961301733a3aa97bee88670
Arslan <marslanmustafa391@gmail.com>
2023-08-02 20:31:11 +0500

    fontAwesome  Added with some style changes

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
+document.getElementById("name").innerHTML = (profile.getName());
     $("#name").text(profile.getName());
+    document.getElementById("name").src = (profile.getImageUrl());
     $("#image").attr('src' ,profile.getImageUrl());
+    document.querySelector(".g-sign-ok").style.display = "flex";
     $(".g-sign-ok").css("display" , "flex");
+    document.querySelector(".g-sign").style.display = "none";
     $(".g-sign").css("display" , "none");
+    document.querySelector(".g-signin2").style.display = "none";
     $(".g-signin2").css("display" , "none");
   }
 
